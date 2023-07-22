b2e1d0978715cc8fa08860cbb11c3df64e8559fe
Sirvance <126606183+Sirvance@users.noreply.github.com>
2023-07-21 20:40:36 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-// function filterOddNumbers(numbers) {
-//   const oddNumbers = numbers.filter(number => number % 2 !== 0);
-//   return oddNumbers.length === 1 ? oddNumbers : oddNumbers;
-// }
+// make an empty array for oddNumbers
+ // loop each element into array and check for odd numbers length
+// return oddNumbers array
+function filterOddNumbers(numbers) {
+   const oddNumbers = numbers.filter(number => number % 2 !== 0);
+   return oddNumbers.length === 1 ? oddNumbers : oddNumbers;
+ }
 
-// // Test cases
-// const arrays = [
-//   [2, 4, 6, 8, 11, 20, 15, 22],
-//   [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-//   [70, 42, 55, 81, 21, 91, 34],
-//   [2, 4, 6, 8, 10, 11, 12]
-// ];
+// // Test 
+ const arrays = [
+  [2, 4, 6, 8, 11, 20, 15, 22],
+   [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  [70, 42, 55, 81, 21, 91, 34],
+  [2, 4, 6, 8, 10, 11, 12]
+ ];
 
 // arrays.forEach(arr => {
 //   const result = filterOddNumbers(arr);
@@ -21,7 +24,12 @@
 
 
 
-
+// define a string with the vowels
+// keep track of the number of consonants and vowels
+// loop through each character in the input string
+// check if character is a vowel if it is increment vowelCount by 1
+// check if character is a letter betwwen "a" and "z"
+// return the word with the counts of consonants and vowels
 function countConsonantsAndVowels(word) {
     const vowels = ["a", "e", "i", "o", "u"];
     let consonantCount = 0;
@@ -38,7 +46,7 @@ function countConsonantsAndVowels(word) {
     return `${word} has ${consonantCount} consonants and ${vowelCount} vowels`;
   }
   
-  // Test cases
+  // Test
   const words = ["hello", "ukelele", "awesome", "onomonopia", "textbook"];
   
   words.forEach(word => {
@@ -54,7 +62,10 @@ function countConsonantsAndVowels(word) {
   
   
   
-  
+  // make an empty array to store reversed elements
+// loop through the input array in reverse order for i from length of inputArr -1 to 0
+// append the current element to the reversedArr append inputArr[i] to reversedArr
+// return the reversed array
   function reverseArray(arr) {
     const reversedArray = [];
   
@@ -65,7 +76,7 @@ function countConsonantsAndVowels(word) {
     return reversedArray;
   }
   
-  // Test cases
+  // Test
   const arrays = [
     [1, 2, 3],
     [1, 3, 5, 7, 9, 11],
